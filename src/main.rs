9adb33ef4824b50e7bf7d6ebbe3807f1e7b8a5f6
Owen Palmer <speedcubegeek@gmail.com>
2022-03-16 19:59:20 -0700

    Basic 2D framwork with points and rects in update loop

---
+++ src/main.rs
@@ -0,0 +1,54 @@
+use::std::{thread, time};
+
+fn main() {
+    let mut canvas = Canvas::new(80,40,".");
+    let mut the_x = 0;
+    let mut update = |test| {
+        canvas.fillRect(0, 0, 80, 40, " ");
+        canvas.point(the_x, 20, "o");
+        the_x = the_x + 1;
+        canvas.display();
+        test
+    };
+    while 1==1 {
+        thread::sleep(time::Duration::from_millis(10));
+        update(1);
+    }
+}
+
+struct Canvas<'a> {
+    width: usize,
+    height: usize,
+    pixels: Box<Vec<Vec<&'a str>>>,
+}
+
+impl<'a> Canvas<'a> {
+    fn new(width: usize, height: usize, fill: &str) -> Canvas {
+        let pixels = Box::new(vec![vec![fill; width]; height]);
+        Canvas { 
+            width,
+            height,
+            pixels,
+        }
+    }
+
+    fn fillRect(&mut self, x: usize, y: usize, w: usize, h: usize, fill: &'a str) {
+        for x_coor in x..x+w {
+            for y_coor in y..y+h {
+                self.pixels[y_coor][x_coor] = fill;
+            }
+        }
+    }
+
+    fn display(&self) {
+        print!("\x1B[2J\x1B[1;1H");
+        for line in self.pixels.iter() {
+            println!("{}", line.join(""));
+        }
+    }
+
+    fn point(&mut self, x: usize, y: usize, draw: &'a str) {
+        self.pixels[y][x] = draw;
+    }
+
+}
